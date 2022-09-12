3551184b19522dc8d7ed4973c0d56af26551b990
fBrown8 <89246233+fBrown8@users.noreply.github.com>
2022-09-12 19:33:20 +0800

    Revert "Finished making pages responsive"

--- js/action-button.js
+++
@@ -1,9 +0,0 @@
-function action(){
-    document.getElementById("submit").innerHTML = "SUBMIT AS VOIDED";
-    document.getElementById("voided").innerHTML = "Submit as Voided";
-}
-
-function resolved(){
-    document.getElementById("submit").innerHTML = "SUBMIT AS RESOLVED";
-    document.getElementById("resolved").innerHTML = "Submit as Resolved";
-}
